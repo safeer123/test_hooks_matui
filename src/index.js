b9e9a202252e5a0a892b34d65c0a493f4e580002
Safeer Chonengal <safeer2c@gmail.com>
2019-05-12 17:58:00 +0530

    Added new font roboto
    Added new font roboto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Typography from "@material-ui/core/Typography";
+import "typeface-roboto";
 
 import BtnPanel from "./components/BtnPanel";
 import Content from "./components/Content";
@@ -9,7 +11,9 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Test App (09-05-2019)</h1>
+      <Typography variant="h4" gutterBottom>
+        Test App (09-05-2019)
+      </Typography>
       <BtnPanel />
       <Content />
     </div>
